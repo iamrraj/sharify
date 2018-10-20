fbf970aff6075eb6ed95ece9595ec14d3f15be26
Piotr Adamczyk <xeno@xeno.yt>
2018-10-20 23:04:55 +0200

    changed overlay to drawer

--- app/App.js
+++ app/App.js
@@ -7,26 +7,11 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import MercariMapView from "./components/MercariMapView";
-import MenuOverlay from "./components/MenuOverlay";
+import { DrawerNavigator } from "react-navigation";
+import HomeScreen from "./components/HomeScreen";
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MercariMapView />
-        <MenuOverlay />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000"
+export default DrawerNavigator({
+  Home: {
+    screen: HomeScreen
   }
 });
---
+++ app/components/HomeScreen.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MercariMapView from "./MercariMapView";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MercariMapView />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000"
+  }
+});
