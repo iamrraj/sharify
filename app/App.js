99988d4ab86c79a1cf31cad93b84f6db5b81c7c9
Piotr Adamczyk <xeno@xeno.yt>
2018-10-20 12:17:18 +0200

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000"
+  }
+});
